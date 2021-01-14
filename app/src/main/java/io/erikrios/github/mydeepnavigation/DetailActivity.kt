60e7616b90ac2934e4a39422e03b36738f69dfdc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-14 09:04:25 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/mydeepnavigation/DetailActivity.kt
+++ app/src/main/java/io/erikrios/github/mydeepnavigation/DetailActivity.kt
@@ -1,11 +1,27 @@
 package io.erikrios.github.mydeepnavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.mydeepnavigation.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
     }
 }
