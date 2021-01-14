58499c08922ccdef60c1e3f09351f2e4e23ecaa6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-14 09:11:41 +0700

    Implements the view binding

--- app/src/main/java/io/erikrios/github/mydeepnavigation/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mydeepnavigation/MainActivity.kt
@@ -1,11 +1,29 @@
 package io.erikrios.github.mydeepnavigation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.mydeepnavigation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOpenDetail.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_open_detail) {
+            val detailIntent = Intent(this@MainActivity, DetailActivity::class.java)
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title))
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message))
+            startActivity(detailIntent)
+        }
     }
 }
